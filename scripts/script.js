c3bbe4a1212608f18dc557440fc4743c588f63e3
Irina-Gorges <r3nategr4nate@gmail.com>
2025-04-21 12:14:42 +0200

    finished project

--- scripts/script.js
+++ scripts/script.js
@@ -1,81 +1,80 @@
-let user = "kommt blad";
 function init() {
-    renderBooks();
+  renderBooks();
 }
 
 function renderBooks() {
-    const contentRef = document.getElementById("content");
-    contentRef.innerHTML = "";
-    for (let i = 0; i < books.length; i++) {
-        contentRef.innerHTML += getBooksTemplates(i);
-        getCommentsTemplates(i);
-    }
+  const contentRef = document.getElementById("content");
+  contentRef.innerHTML = "";
+  for (let i = 0; i < books.length; i++) {
+    contentRef.innerHTML += getBooksTemplates(i);
+    getCommentsTemplates(i);
+  }
 }
 
 function getCommentsTemplates(i) {
-    let commentsRef = document.getElementById(`bookscomm${i}`);
-    for (let c = 0; c < books[i].comments.length; c++) {
-        commentsRef.innerHTML += `<p>Name:${books[i].comments[c].name}</p><p>Kommentare:${books[i].comments[c].comment}</p>`;
-    }
+  let commentsRef = document.getElementById(`bookscomm${i}`);
+  for (let c = 0; c < books[i].comments.length; c++) {
+    commentsRef.innerHTML += `<p>Name:${books[i].comments[c].name}</p><p>Kommentare:${books[i].comments[c].comment}</p>`;
+  }
 }
 
 function renderHeart(i) {
-    let heartRef = books[i];
+  let heartRef = books[i];
 
-    if (heartRef.liked == true) {
-        return `<img id="btn" src="img/heart_like.png" onclick="renderHeartView(${i})" alt="Like"></img>`;
-    } else {
-        return `<img id="btn" src="img/heart_notlike.png" onclick="renderHeartView(${i})" alt="Like"></img>`;
-    }
+  if (heartRef.liked == true) {
+    return `<img id="btn" src="img/heart_like.png" onclick="renderHeartView(${i})" alt="Like"></img>`;
+  } else {
+    return `<img id="btn" src="img/heart_notlike.png" onclick="renderHeartView(${i})" alt="Like"></img>`;
+  }
 }
 
 function renderHeartView(i) {
-    let heartviewRef = books[i];
+  let heartviewRef = books[i];
 
-    if (heartviewRef.liked == true) {
-        books[i].liked = false;
-        books[i].likes -= 1;
-    } else {
-        books[i].liked = true;
-        books[i].likes += 1;
-    }
-    renderBooks();
+  if (heartviewRef.liked == true) {
+    books[i].liked = false;
+    books[i].likes -= 1;
+  } else {
+    books[i].liked = true;
+    books[i].likes += 1;
+  }
+  renderBookContent(i);
 }
 
 function userComment(i) {
-    let userRef = user;
-    let userInputRef = document.getElementById("userinput");
+  let userInputRef = document.getElementById(`userinput${i}`);
+  let userRef = document.getElementById("username").value;
+  let errorMessage = document.getElementById("usererror");
+  errorMessage.innerHTML = "";
+
+  if (userRef != "") {
     books[i].comments.push({ name: userRef, comment: userInputRef.value });
+
     renderBooks();
+    toggleClose();
+  } else {
+    errorMessage.innerHTML = "Bitte Namen angeben!";
+  }
 }
 
-function toggleOverlay() {
-    // Funktion zum Öffnen des User Eingabefelds im Dialog
-    const dialogUserRef = document.getElementById("main_container");
-
-    let overlayRef = document.getElementById("overlay");
-
-    overlayRef.innerHTML = "";
-    overlayRef.innerHTML = `<div onclick="bubblingprotection(event)" id="dialog">
-              <div id="username">
-        <input type="text" placeholder="Username eintragen">
-        <button id="btn_username" onclick="toogleOverlay">Senden</button>
-       </div></div>`;
-
-    toggleClose();
+function toggleOverlay(i) {
+  let overlayRef = document.getElementById("overlay");
+  overlayRef.innerHTML = "";
+  overlayRef.innerHTML = getToogleOverlay(i);
+  toggleClose();
 }
 
 function toggleClose() {
-    let overlayRef = document.getElementById("overlay");
-    overlayRef.classList.toggle("d_none");
+  let overlayRef = document.getElementById("overlay");
+  overlayRef.classList.toggle("d_none");
 }
 
 function bubblingprotection(event) {
-    // console.log("dialog");
-    event.stopPropagation();
+  event.stopPropagation();
 }
 
-// Logo einbauen
-// Overlay erstellen
-// UserName Eingabefeld setzen
-// hübsch machen
+function renderBookContent(i) {
+  let contentRef = document.getElementById(`bookcontent${i}`);
+  contentRef.innerHTML = getBooksContentTemplates(i);
+  getCommentsTemplates(i);
+}
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,19 +1,47 @@
 function getBooksTemplates(i) {
-    return (
-        `<div id="bookcontent${i}" class="book_container"><p>Titel:${books[i].name} 
-    <img src="img/${books[i].name}.webp"></p>
-    <p>von&nbsp;${books[i].author}</p>
+  return (
+    `<div id="bookcontent${i}" class="book_container"><h3>${books[i].name}</h3>
+    <img src="img/${books[i].name}.webp">
+    <p class="author">von&nbsp;${books[i].author}</p>
     <div id="like"><p>${books[i].likes}` +
-        renderHeart(i) +
-        `</p></div>
-    <p>Preis:${books[i].price}€</p>
-    <p>Veröffentlicht:${books[i].publishedYear}</p>
-    <p>Genre:${books[i].genre}</p><br>
+    renderHeart(i) +
+    `</p></div>
+    <div id="bookinfo"><p>Preis:${books[i].price}€&nbsp;|&nbsp;</p>
+    <p>Veröffentlicht:${books[i].publishedYear}&nbsp;|&nbsp;</p>
+    <p>${books[i].genre}</p></div><hr>
     <div id="bookscomm${i}" class="bcomm"><p></p>
         </div>
         <div id="usercomm">
-                <input id="userinput" type="text" max="100">
-                <button id="usercommbtn" onclick="userComment(${i})">Senden</button>
+                <input id="userinput${i}" class="user_input" type="text" max="100">
+                <button id="usercommbtn" onclick="toggleOverlay(${i})">Senden</button>
             </div></div>`
-    );
+  );
+}
+
+function getToogleOverlay(i) {
+  return `<div onclick="bubblingprotection(event)" id="dialog">
+              <div>
+        <input id="username" class="user_name" type="text" placeholder="Username eintragen">
+        <button id="btn_username" onclick="userComment(${i})">Senden</button>
+       </div><div id="usererror"></div></div>`;
+}
+
+function getBooksContentTemplates(i) {
+  return (
+    `<h3>${books[i].name}</h3>
+    <img src="img/${books[i].name}.webp">
+    <p class="author">von&nbsp;${books[i].author}</p>
+    <div id="like"><p>${books[i].likes}` +
+    renderHeart(i) +
+    `</p></div>
+    <div id="bookinfo"><p>Preis:${books[i].price}€&nbsp;|&nbsp;</p>
+    <p>Veröffentlicht:${books[i].publishedYear}&nbsp;|&nbsp;</p>
+    <p>${books[i].genre}</p></div><hr>
+    <div id="bookscomm${i}" class="bcomm"><p></p>
+        </div>
+        <div id="usercomm">
+                <input id="userinput${i}" class="user_input" type="text" max="100">
+                <button id="usercommbtn" onclick="toggleOverlay(${i})">Senden</button>
+            </div>`
+  );
 }
